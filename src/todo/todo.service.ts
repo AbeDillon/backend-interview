8edae3b4f4e078af3dc2c56c507064b320daae4c
AbeDillon <abedillon@gmail.com>
2021-03-11 12:25:46 -0600

    added crud service methods

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepo: Repository<Todo>
+  ) {}
+  async create(todo: Todo): Promise<Todo> {
+    return await this.todoRepo.save(todo);
+  }
+  async retrieveAll(): Promise<Todo[]> {
+    return await this.todoRepo.find();
+  }
+  async update(todo: Todo): Promise<UpdateResult> {
+    return await this.todoRepo.update(todo.id, todo);
+  }
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.todoRepo.delete(id);
+  }
+}
