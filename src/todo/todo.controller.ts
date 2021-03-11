0580b337fd6f96b5c1041ced8b235fb0becf41a4
AbeDillon <abedillon@gmail.com>
2021-03-11 15:50:08 -0600

    added endpoint for retrieving todos

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { Todo } from './todo.entity';
+import { TodoService} from './todo.service';
 
 
 @Controller()
 export class TodoController {
-  
+  constructor(private todoService: TodoService) {}
   @Get()
-  public getTodoList() {
-    return "<u>My Todo List</u>";
+  public getTodoList(): Promise<Todo[]> {
+    return this.todoService.retrieveAll();
   }
 }
